cc8b1409c560d34e9829c5279b8438991fba8e1b
antanim <abdurnouutanim11@gmail.com>
2022-02-15 00:51:06 +0600

    32 minute done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,10 @@
 import InsertCommentIcon from "@material-ui/icons/InsertComment";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
 import TollIcon from "@material-ui/icons/Toll";
 import React from "react";
 import userLogo from "../assets/user.png";
+import "../styles/Sidebar.css";
 
 const Sidebar = () => {
   return (
@@ -11,10 +13,17 @@ const Sidebar = () => {
         <div className="sidebar__header__img">
           <img src={userLogo} alt="user" />
         </div>
-        <div className="sidebar__header__btn">
+        <div className=" sidebar__header__btn">
           <TollIcon />
           <InsertCommentIcon />
           <MoreVertIcon />
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__search__input">
+          <SearchIcon />
+          <input type="text" name="search" placeholder="Search..." />
         </div>
       </div>
     </div>
